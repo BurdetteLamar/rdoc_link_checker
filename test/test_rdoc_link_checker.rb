474d495c815820384488388c57376862108c3f7d
BurdetteLamar <burdettelamar@yahoo.com>
2023-05-21 17:29:55 +0100

    Testing

--- test/test_rdoc_link_checker.rb
+++ test/test_rdoc_link_checker.rb
@@ -18,17 +18,54 @@ class TestRDocLinkChecker < Minitest::Test
           ['no_toc', no_toc.to_s],
         ]
         doc = run_link_checker('test/html', onsite_only, no_toc)
-        table = doc.xpath('table[@id="parameters"]')
+        table = doc.xpath("//table[@id='parameters']")
         table.search('tr').each_with_index do |row, i|
           texts = row.search('th, td').map { |cell| cell.text.strip }
           assert_equal(exp_texts[i], texts)
         end
       end
     end
-
   end
 
-  def run_link_checker(html_dirpath, onsite_only, no_toc)
+  def test_times_table
+    exp_texts = [
+      'Times',
+      'Start Time',
+      'End Time',
+      'Elapsed Time'
+    ]
+    doc = run_link_checker('test/html')
+    table = doc.xpath("//table[@id='times']")
+    table.search('tr').each_with_index do |row, i|
+      texts = row.search('th, td').map { |cell| cell.text.strip }
+      assert_equal(exp_texts[i], texts[0])
+      exp = i == 0 ? 1 : 2
+      assert_equal(exp, texts.size)
+    end
+  end
+
+  def test_counts_table
+    exp_texts = [
+      'Counts',
+      'Source Pages',
+      'Target Pages',
+      'Links Checked',
+      'Links Broken'
+    ]
+    doc = run_link_checker('test/html')
+    table = doc.xpath("//table[@id='counts']")
+    table.search('tr').each_with_index do |row, i|
+      texts = row.search('th, td').map { |cell| cell.text.strip }
+      assert_equal(exp_texts[i], texts[0])
+      exp = i == 0 ? 1 : 2
+      assert_equal(exp, texts.size)
+      next if i == 0
+      count = Integer(texts[1])
+      assert_operator(count, :>=, 0)
+    end
+  end
+
+  def run_link_checker(html_dirpath, onsite_only = true, no_toc = true)
     command = "ruby bin/rdoc_link_checker #{html_dirpath}"
     command += ' --onsite_only' if onsite_only
     command += ' --no_toc' if no_toc
